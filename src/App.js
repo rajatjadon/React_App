66aa5ce52426fdb59159d63d8b6b1ebdaa2d3144
rajatjadon <rajatjadon566@gmail.com>
2024-11-11 10:25:02 +0530

    uppercase converter added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import TextArea from "./Components/TextArea";
 
 function App() {
   return (
     <>
       <Navbar />
       {/* <Navbar title="React App" about='About React'/> */}
+      <div className="container">
+        <TextArea heading="Enter your Text below"/>
+      </div>
     </>
   );
 }
